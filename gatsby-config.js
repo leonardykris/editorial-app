958befcf5c4a022b4a30b5cf19178dede3fd83d1
Leonardy <bisa.delivery@gmail.com>
2020-07-28 01:55:02 +0700

    initial top 10 content

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,27 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-FNZ06CQ57R",
+      }
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Editorial`,
+    author: `Leonardy Kristianto`,
+    description: `Opinions of curated articles and interviews`,
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        name: `Twitter`,
+        url: `https://twitter.com/leonardykris`,
       },
       {
-        name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        name: `LinkedIn`,
+        url: `https://linkedin.com/in/leonardykris`,
       },
     ],
   },
--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -8,11 +8,7 @@ import { Styled } from "theme-ui"
 export default function Bio() {
   return (
     <>
-      This is where <Styled.a href="http://example.com/">your name</Styled.a>
-      {` `}
-      goes.
-      <br />
-      Or whatever, you make the rules.
+      <Styled.a href="http://example.com/">Leonardy Kristianto</Styled.a>
     </>
   )
 }
